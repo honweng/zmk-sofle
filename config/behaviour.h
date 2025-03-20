c05c7c78937b5e920961645b62f577edb7d1861a
honweng <yeehonweng@gmail.com>
2025-03-20 22:36:02 +0800

    Miryoku

--- config/behaviour.h
+++ config/behaviour.h
@@ -5,3 +5,9 @@
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
 
+#define U_HML(MOD, TAP) &hm_l MOD TAP
+#define U_HMR(MOD, TAP) &hm_r MOD TAP
+
+#define U_HSL(MOD, TAP) &hm_sl MOD TAP
+#define U_HSR(MOD, TAP) &hm_sr MOD TAP
+
